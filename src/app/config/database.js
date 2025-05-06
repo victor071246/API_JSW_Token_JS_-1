57ac41df90e077628313a2e6dbc25ab1769b81fd
victor071246 <daderiovictorfelipe@gmail.com>
2025-05-05 23:59:07 -0300

    second commit

---
+++ src/app/config/database.js
@@ -0,0 +1,11 @@
+export default {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'Vv@2802302207',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
